8e187b987663c0f211eddf2c165f7987f4862807
iambocai bob.chen.cs@gmail.com <bob.chen.cs@gmail.com>
2016-04-26 22:24:25 +0800

    Update README.md

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ falcon-mailer
 
 falcon-sender 发送邮件到smtp服务器的接口实现，当然你也可以把它当做一个简单的httpmail接口使用也没问题:)
 
-安装
+##安装
 --------------------
 
 ```bash
@@ -22,14 +22,71 @@ chmod 755 control
 # if anything goes wrong, check log for reasons
 ```
 
-配置
+##配置
 -----------------------
 
 - whitelist: 允许发起http调用的客户端IP地址或CIDR格式的范围列表，逗号分隔
 - spliter: tos参数中，收件人地址之间的分隔符号
+- maxbytes: 发送带附件的邮件时，允许附件最大多大？
 - 其它应该都认识吧。。
 
-FAQ
+##API
+
+####发送普通邮件
+
+**URL**: /api/mail
+
+**请求方式**：POST
+
+**Content_type**: application/x-www-form-urlencoded
+
+|参数|必需|含义|备注|
+|---|----|---|---|
+|tos|是|	邮件收件人列表|多个用英文逗号,分割，分割符号可以在配置中修改|
+|subject|是|邮件主题|请使用utf8编码|
+|content|是|邮件正文|请使用utf8编码|
+|format|否|邮件正文的格式|默认为text，可设置为text或html|
+|from|否|发件人|不能含有非ASCII字符，不建议设置|
+|server|否|自定义SMTP服务器，格式如：smtp.exmail.qq.com:25|如要自定义则server，user，passwd必须同时设定，否则使用配置中的信息|
+|user|否|登陆邮箱使用的用户名，如abc@qq.com|如要自定义则server，user，passwd必须同时设定，否则使用配置中的信息|
+|passwd|否|登陆邮箱使用的密码，如123456|如要自定义则server，user，passwd必须同时设定，否则使用配置中的信息|
+
+
+
+**请求响应：**
+
+成功：ok
+失败：错误消息
+
+####发送带附件的邮件
+
+**URL**: /api/attachmail
+
+**请求方式**：POST
+
+**Content_type**: multipart/form-data
+
+|参数|必需|含义|备注|
+|---|----|---|---|
+|tos|是|	邮件收件人列表|多个用英文逗号,分割，分割符号可以在配置中修改|
+|subject|是|邮件主题|请使用utf8编码|
+|content|是|邮件正文|请使用utf8编码|
+|attachments|是|附件文件|请注意不要超过配置中设置的文件大小限制|
+|format|否|邮件正文的格式|默认为text，可设置为text或html|
+|from|否|发件人|不能含有非ASCII字符，不建议设置|
+|server|否|自定义SMTP服务器，格式如：smtp.exmail.qq.com:25|如要自定义则server，user，passwd必须同时设定，否则使用配置中的信息|
+|user|否|登陆邮箱使用的用户名，如abc@qq.com|如要自定义则server，user，passwd必须同时设定，否则使用配置中的信息|
+|passwd|否|登陆邮箱使用的密码，如123456|如要自定义则server，user，passwd必须同时设定，否则使用配置中的信息|
+
+
+
+**请求响应：**
+
+成功：ok
+失败：错误消息
+
+
+##FAQ
 -----------------------
 
 1. 我想用QQ/163/yahoo/gmail邮箱发信，smtp怎么配置？
--- http/smtp.go
+++ http/smtp.go
@@ -13,107 +13,125 @@ import (
 
 func configSmtpRoutes() {
 	http.HandleFunc("/api/mail", func(w http.ResponseWriter, r *http.Request) {
-		addr := strings.Split(r.RemoteAddr, ":")[0]
-		allowList := strings.Split(g.Config().Http.WhiteList, ",")
-		authorized := isValid(addr, allowList)
-
-		//授权检查
-		if authorized == false {
-			http.Error(w, "remote not in whitelist", http.StatusBadRequest)
-			return
-		}
-
-		if r.ContentLength == 0 {
-			http.Error(w, "body is blank", http.StatusBadRequest)
-			return
-		}
-
-		err := r.ParseMultipartForm(g.Config().Smtp.MaxBytes)
-
-		if err != nil {
-			http.Error(w, "param prase error", http.StatusBadRequest)
-			return
-		}
-
-		if len(r.Form["tos"]) == 0 || len(r.Form["content"]) == 0 || len(r.Form["subject"]) == 0 {
-			http.Error(w, "param tos,content or subject lost", http.StatusBadRequest)
-			return
-		}
-
-		e := email.NewEmail()
-		//收件人
-		e.To = strings.Split(r.Form.Get("tos"), g.Config().Smtp.Spliter)
-		//主题
-		e.Subject = r.Form.Get("subject")
-
-		//抄送，可选
-		if len(r.Form.Get("cc")) != 0 {
-			e.Cc = strings.Split(r.Form.Get("cc"), g.Config().Smtp.Spliter)
-		}
-
-		//密送，可选
-		if len(r.Form.Get("bcc")) != 0 {
-			e.Bcc = strings.Split(r.Form.Get("bcc"), g.Config().Smtp.Spliter)
-		}
-
-		//消息体格式，可选。默认为text，如果format设置为html，则以html方式发送
-		if len(r.Form.Get("format")) != 0 && r.Form.Get("format") == "html" {
-			e.HTML = []byte(r.Form.Get("content"))
-
-		} else {
-			e.Text = []byte(r.Form.Get("content"))
-		}
-
-		//附件，可选
-		if len(r.Form.Get("hasAttach")) != 0 && r.Form.Get("hasAttach") == "1" {
-			m := r.MultipartForm
-
-			//get the *fileheaders
-			files := m.File["attachments"]
-			for i, _ := range files {
-				//for each fileheader, get a handle to the actual file
-				file, err := files[i].Open()
-				defer file.Close()
-				if err != nil {
-					http.Error(w, "open attach file stream error", http.StatusInternalServerError)
-					return
-				}
-				//attach each file to email
-				e.Attach(file, files[i].Filename, "")
-			}
-		}
-
-		var server, user, passwd string
-		//smtp服务器，如果没有则使用默认配置
-		if len(r.Form.Get("server")) != 0 && len(r.Form.Get("user")) != 0 && len(r.Form.Get("passwd")) != 0 {
-			server = r.Form.Get("server")
-			user = r.Form.Get("user")
-			passwd = r.Form.Get("passwd")
-		} else {
-			server = g.Config().Smtp.Addr
-			user = g.Config().Smtp.User
-			passwd = g.Config().Smtp.Pass
-		}
-
-		//发件人，可以设置成 San Zhang <zhangsan@example.com> 这种形式(注意不能有非ascii字符)，如果不设置，默认为登陆使用的账号
-		if len(r.Form.Get("from")) != 0 {
-			e.From = r.Form.Get("from")
-		} else {
-			e.From = user
-		}
-
-		hp := strings.Split(server, ":")
-		auth := smtp.PlainAuth("", user, passwd, hp[0])
-		//暂时不支持TLS/StartTLS等加密认证
-		error := e.Send(server, auth)
-		if error != nil {
-			log.Println("[ERROR]", addr, e.From, e.To, e.Subject, error)
-			http.Error(w, error.Error(), http.StatusBadRequest)
-			return
-		}
-		log.Println("[INFO]", addr, e.From, e.To, e.Subject)
-		w.Write([]byte("{\"status\":0,\"msg\":\"ok\"}"))
+		SendMailBySmtp(w, r, false)
 	})
+	http.HandleFunc("/api/attachmail", func(w http.ResponseWriter, r *http.Request) {
+		SendMailBySmtp(w, r, true)
+	})
+
+}
+
+func SendMailBySmtp(w http.ResponseWriter, r *http.Request, hasAttach bool) {
+	var (
+		err    error
+		server string
+		user   string
+		passwd string
+	)
+
+	addr := strings.Split(r.RemoteAddr, ":")[0]
+	allowList := strings.Split(g.Config().Http.WhiteList, ",")
+	authorized := isValid(addr, allowList)
+
+	//授权检查
+	if authorized == false {
+		http.Error(w, "remote not in whitelist", http.StatusBadRequest)
+		return
+	}
+
+	if r.ContentLength == 0 {
+		http.Error(w, "body is blank", http.StatusBadRequest)
+		return
+	}
+
+	//根据是否有附件，使用不同的ParseForm方法
+	if hasAttach == true {
+		err = r.ParseMultipartForm(g.Config().Smtp.MaxBytes)
+	} else {
+		err = r.ParseForm()
+	}
+	if err != nil {
+		http.Error(w, "param prase error", http.StatusBadRequest)
+		return
+	}
+
+	if len(r.Form["tos"]) == 0 || len(r.Form["content"]) == 0 || len(r.Form["subject"]) == 0 {
+		http.Error(w, "param tos,content or subject lost", http.StatusBadRequest)
+		return
+	}
+
+	e := email.NewEmail()
+	//收件人
+	e.To = strings.Split(r.Form.Get("tos"), g.Config().Smtp.Spliter)
+	//主题
+	e.Subject = r.Form.Get("subject")
+
+	//抄送，可选
+	if len(r.Form.Get("cc")) != 0 {
+		e.Cc = strings.Split(r.Form.Get("cc"), g.Config().Smtp.Spliter)
+	}
+
+	//密送，可选
+	if len(r.Form.Get("bcc")) != 0 {
+		e.Bcc = strings.Split(r.Form.Get("bcc"), g.Config().Smtp.Spliter)
+	}
+
+	//消息体格式，可选。默认为text，如果format设置为html，则以html方式发送
+	if len(r.Form.Get("format")) != 0 && r.Form.Get("format") == "html" {
+		e.HTML = []byte(r.Form.Get("content"))
+
+	} else {
+		e.Text = []byte(r.Form.Get("content"))
+	}
+
+	//附件，可选
+	if hasAttach == true {
+		m := r.MultipartForm
+
+		//get the *fileheaders
+		files := m.File["attachments"]
+		for i, _ := range files {
+			//for each fileheader, get a handle to the actual file
+			file, err := files[i].Open()
+			defer file.Close()
+			if err != nil {
+				http.Error(w, "open attach file stream error", http.StatusInternalServerError)
+				return
+			}
+			//attach each file to email
+			e.Attach(file, files[i].Filename, "")
+		}
+	}
+
+	//smtp服务器，如果没有则使用默认配置
+	if len(r.Form.Get("server")) != 0 && len(r.Form.Get("user")) != 0 && len(r.Form.Get("passwd")) != 0 {
+		server = r.Form.Get("server")
+		user = r.Form.Get("user")
+		passwd = r.Form.Get("passwd")
+	} else {
+		server = g.Config().Smtp.Addr
+		user = g.Config().Smtp.User
+		passwd = g.Config().Smtp.Pass
+	}
+
+	//发件人，可以设置成 San Zhang <zhangsan@example.com> 这种形式(注意不能有非ascii字符)，如果不设置，默认为登陆使用的账号
+	if len(r.Form.Get("from")) != 0 {
+		e.From = r.Form.Get("from")
+	} else {
+		e.From = user
+	}
+
+	hp := strings.Split(server, ":")
+	auth := smtp.PlainAuth("", user, passwd, hp[0])
+	//暂时不支持TLS/StartTLS等加密认证
+	error := e.Send(server, auth)
+	if error != nil {
+		log.Println("[ERROR]", addr, e.From, e.To, e.Subject, error)
+		http.Error(w, error.Error(), http.StatusBadRequest)
+		return
+	}
+	log.Println("[INFO]", addr, e.From, e.To, e.Subject)
+	w.Write([]byte("{\"status\":0,\"msg\":\"ok\"}"))
 }
 
 //检查调用IP合法性函数
